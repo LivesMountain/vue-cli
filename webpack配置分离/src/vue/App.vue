4746d8b11ed48cde9009ad1aca4c82b6914aa08d
likun <likun@wangxiaobao.com>
2020-09-22 11:57:25 +0800

    配置文件分离

---
+++ webpack配置分离/src/vue/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>我是一个脚手架{{message}}</h2>
+        <button @click='click'>按钮</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"App",
+        data(){
+            return {
+                message:'6666'
+            }
+        },
+        methods:{
+        click(){
+            console.log(this.message);
+        }
+        }
+    }
+
+</script>
+
+
+
+<style scoped>
+    .h2 {
+        color:green;
+    }
+</style>
