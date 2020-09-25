e9c688582a08256d068378c7a987efab668cc9c2
likun <likun@wangxiaobao.com>
2020-09-25 19:16:07 +0800

    tab-bar

---
+++ tabbar/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+    
+<style>
+  @import "./assets/css/base.css";
+  
+
+  #tab-bar {
+    display: flex;
+  }
+
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+</style>
---
+++ tabbar/src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+  ]
+})
+// require('../assets/css/base.css')
